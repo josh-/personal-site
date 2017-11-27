15b012625cfe3d35f5629933e21a875dfe8c2828
Josh Parnham <joshparnham@gmail.com>
2017-11-26 20:47:48 -0800

    Update apt-get commands in certbot script

--- certbot.sh
+++ certbot.sh
@@ -1,6 +1,6 @@
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install certbot
+apt-get update
+add-apt-repository ppa:certbot/certbot
+apt-get -y install certbot
 
 sudo certbot certonly --manual \
     --non-interactive \
