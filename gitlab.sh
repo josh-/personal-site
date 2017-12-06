815fcb03aa4d646b4aa6cb26c805bfb2ef15e2ef
Josh Parnham <joshparnham@gmail.com>
2017-12-06 14:25:23 -0500

    Wrap interoperated form data in single quotes

--- gitlab.sh
+++ gitlab.sh
@@ -3,6 +3,6 @@ KEY_PEM="/etc/letsencrypt/live/$LE_DOMAIN/privkey.pem"
 
 curl --request PUT \
     --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
-    --form="certificate=@$CERT_PEM" \
-    --form="key=@$KEY_PEM" \
+    --form="'certificate=@$CERT_PEM'" \
+    --form="'key=@$KEY_PEM'" \
     https://gitlab.com/api/v4/projects/$GITLAB_USERNAME%2F$GITLAB_PROJECT/pages/domains/$LE_DOMAIN
