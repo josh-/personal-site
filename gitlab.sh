40aa00768fac1d35125024079e8a958a596f1a16
Josh Parnham <joshparnham@gmail.com>
2017-12-06 14:12:47 -0500

    Upload file data instead of inlining into command

--- gitlab.sh
+++ gitlab.sh
@@ -1,8 +1,8 @@
-CERT_PEM=$(cat /etc/letsencrypt/live/$LE_DOMAIN/cert.pem)
-KEY_PEM=$(cat /etc/letsencrypt/live/$LE_DOMAIN/privkey.pem)
+CERT_PEM="/etc/letsencrypt/live/$LE_DOMAIN/cert.pem"
+KEY_PEM="/etc/letsencrypt/live/$LE_DOMAIN/privkey.pem"
 
 curl --request PUT \
     --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
-    --form="certificate=$CERT_PEM" \
-    --form="key=$KEY_PEM" \
+    --form="certificate=@$CERT_PEM" \
+    --form="key=@$KEY_PEM" \
     https://gitlab.com/api/v4/projects/$GITLAB_USERNAME%2F$GITLAB_PROJECT/pages/domains/$LE_DOMAIN
