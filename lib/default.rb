b9b328c208f0a898ac289635ac3e3a744ddebdb1
Josh Parnham <joshparnham@gmail.com>
2017-12-14 15:11:09 +1100

    Include Breadcrumbs helper

--- lib/default.rb
+++ lib/default.rb
@@ -4,6 +4,7 @@ include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Breadcrumbs
 
 module PostHelper
   def formatted_date(post)
