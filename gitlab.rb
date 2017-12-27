63ad9fad0c32dcf8640ee3f5d477e954400423d3
Josh Parnham <joshparnham@gmail.com>
2017-12-27 13:00:32 +1100

    Include JSON parsing library and update GitLab API to full URL

--- gitlab.rb
+++ gitlab.rb
@@ -1,23 +1,26 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
 
 http = Net::HTTP.new('gitlab.com', 443)
 http.use_ssl = true
 
 token_headers = { 'PRIVATE-TOKEN' => ENV['GITLAB_TOKEN'] }
 
-request = Net::HTTP::Put.new("api/v4/projects/#{ENV['GITLAB_USERNAME']}%2F#{ENV['GITLAB_PROJECT']}/pages/domains/#{ENV['LE_DOMAIN']}", token_headers)
+request = Net::HTTP::Put.new("https://gitlab.com/api/v4/projects/#{ENV['GITLAB_USERNAME']}%2F#{ENV['GITLAB_PROJECT']}/pages/domains/#{ENV['LE_DOMAIN']}", token_headers)
 
-content_headers = {
+certificate_data = {
   'certificate' => IO.read("./certbot/config/live/#{ENV['LE_DOMAIN']}/fullchain.pem"),
   'key' => IO.read("./certbot/config/live/#{ENV['LE_DOMAIN']}/privkey.pem")
 }
 
-request.set_form_data(content_headers)
+request.set_form_data(certificate_data)
+
+# http.set_debug_output $stderr
+
+puts request.body
+
 update_cert_response = http.request(request)
-
-puts update_cert_response.code
-puts update_cert_response.message
-
-update_data = JSON.parse(update_cert_response.body)
