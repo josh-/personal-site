6dd5df34c15709e472cfbb1a3819fc206c1a554f
Josh Parnham <joshparnham@gmail.com>
2019-07-17 21:01:19 +1000

    Remove custom LE implementation

--- gitlab.sh
+++
@@ -1,8 +0,0 @@
-CERT_PEM=$(readlink -f "./certbot/config/live/$LE_DOMAIN/fullchain.pem")
-KEY_PEM=$(readlink -f "./certbot/config/live/$LE_DOMAIN/privkey.pem")
-
-curl --request PUT \
-    --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
-    --form "'certificate=@$CERT_PEM'" \
-    --form "'key=@$KEY_PEM'" \
-    https://gitlab.com/api/v4/projects/$GITLAB_USERNAME%2F$GITLAB_PROJECT/pages/domains/$LE_DOMAIN
